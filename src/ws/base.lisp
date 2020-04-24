1d0e643130e8ad8a272cfbd212d5d2bef84217ba
eta <hi@theta.eu.org>
2020-04-24 15:11:11 +0100

    Remove broken HANDLER-CASEs in READ-WEBSOCKET-FRAME
    - It doesn't make sense to spin in a loop when an error is encountered.
      As such, the HANDLER-CASEs were removed to allow the caller to handle the
      error (in most cases, the connection just closes)

--- src/ws/base.lisp
+++ src/ws/base.lisp
@@ -211,35 +211,28 @@
   (let ((buf (make-array 2 :element-type '(unsigned-byte 8)))
         (extended-buf (make-array 8 :element-type '(unsigned-byte 8))))
     (block nil
-      (tagbody retry
-         (let ((read-bytes (handler-case (read-sequence buf stream)
-                             (error ()
-                               ;; Retry when I/O timeout error
-                               (go retry)))))
-           (when (= read-bytes 0)
-             (return nil))
+      (let ((read-bytes (read-sequence buf stream)))
+        (when (= read-bytes 0)
+          (return nil))
 
-           (let ((maskp (plusp (ldb (byte 1 7) (aref buf 1))))
-                 (data-length (ldb (byte 7 0) (aref buf 1))))
-             (cond
-               ((<= 0 data-length 125))
-               (t
-                (let ((end (if (= data-length 126) 2 8)))
-                  (read-sequence extended-buf stream :end end)
-                  (incf read-bytes end)
-                  (setf data-length
-                        (loop with length = 0
-                              for i from 0 below end
-                              do (setf length (+ (ash length 8) (aref extended-buf i)))
-                              finally (return length))))))
-             (when maskp
-               (incf data-length 4))
-             (let ((data (make-array (+ read-bytes data-length) :element-type '(unsigned-byte 8))))
-               (replace data buf :end2 2)
-               (unless (= read-bytes 2)
-                 (replace data extended-buf :start1 2 :end2 (- read-bytes 2)))
-               (handler-case
-                   (read-sequence data stream :start read-bytes)
-                 (error ()
-                   (return nil)))
-               (return data))))))))
+        (let ((maskp (plusp (ldb (byte 1 7) (aref buf 1))))
+              (data-length (ldb (byte 7 0) (aref buf 1))))
+          (cond
+            ((<= 0 data-length 125))
+            (t
+             (let ((end (if (= data-length 126) 2 8)))
+               (read-sequence extended-buf stream :end end)
+               (incf read-bytes end)
+               (setf data-length
+                     (loop with length = 0
+                           for i from 0 below end
+                           do (setf length (+ (ash length 8) (aref extended-buf i)))
+                           finally (return length))))))
+          (when maskp
+            (incf data-length 4))
+          (let ((data (make-array (+ read-bytes data-length) :element-type '(unsigned-byte 8))))
+            (replace data buf :end2 2)
+            (unless (= read-bytes 2)
+              (replace data extended-buf :start1 2 :end2 (- read-bytes 2)))
+            (read-sequence data stream :start read-bytes)
+            (return data)))))))
